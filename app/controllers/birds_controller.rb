3fb9c3ad7a74fdfc8ee99295067bc7c4dc210a4d
George Baroi <baroi.george@gmail.com>
2022-09-22 14:15:19 -0400

    done with assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,9 +8,16 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(birds_params)
     render json: bird, status: :created
   end
+
+  private
+  def birds_params
+   params.permit(:name, :species)
+  end
+end
+
 
   # GET /birds/:id
   def show
